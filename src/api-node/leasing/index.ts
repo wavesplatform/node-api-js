5dc2c8094d527f9dfcc4a3237b799206dfdd031f
Daniil <lobanovdanil97@gmail.com>
2021-05-17 13:52:18 +0300

    fetchLeasingInfo

--- src/api-node/leasing/index.ts
+++ src/api-node/leasing/index.ts
@@ -14,3 +14,24 @@ export function fetchActive(base: string, address: string, options: RequestInit 
 export interface IActive {
     feeAssetId: string | null;
 }
+
+/**
+ * GET /leasing/info/
+ * Get lease transactions info.
+ */
+export function fetchLeasingInfo(base: string, ids: string[], options: RequestInit = Object.create(null)): Promise<Array<ILeaseInfo>> {
+    const searchParams = new URLSearchParams(ids.reduce((acc, id) => [...acc, ["id", id]], [] as string[][]))
+    return request({ base, url: `/leasing/active/`, options: {...options, body: searchParams} });
+}
+
+export interface ILeaseInfo {
+    id: string,
+    originTransactionId: string,
+    sender: string,
+    recipient: string,
+    amount: string | number,
+    height: number,
+    status: string,
+    cancelHeight: number,
+    cancelTransactionId: string
+}
