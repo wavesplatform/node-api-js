00a9f1d52904ac7ba765de932693754a155853e4
Daniil <lobanovdanil97@gmail.com>
2021-05-17 19:56:09 +0300

    fixed

--- src/tools/parse.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var reg = new RegExp('((?!\\\\)"\\w+"):\\s*(-?[\\d|\\.]{14,})', 'g');
-function default_1(json) {
-    return JSON.parse(json.replace(reg, "$1:\"$2\""));
-}
-exports["default"] = default_1;
-//# sourceMappingURL=parse.js.map
--- src/tools/resolve.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function default_1(path, base) {
-    return new URL(path, base).toString();
-}
-exports["default"] = default_1;
-//# sourceMappingURL=resolve.js.map
