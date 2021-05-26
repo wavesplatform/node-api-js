c1de2d4ad694e7cf90e89b24384c69284c4667c8
enichegovskiy <enichegovskiy@web3tech.ru>
2021-05-26 17:13:44 +0300

    addresses spec

--- test/api-node/addresses.spec.ts
+++ test/api-node/addresses.spec.ts
@@ -1,7 +1,7 @@
-import { NODE_URL, STATE } from '../_state';
-import { create } from '../../src';
-import { IScriptInfo, fetchScriptInfo } from '../../src/api-node/addresses';
-import { isNullableStringOrNumber, isStringOrNumber } from  '../extendedMatcher'
+import {MASTER_ACCOUNT, NODE_URL, STATE} from '../_state';
+import {create} from '../../src';
+import {IScriptInfo, fetchScriptInfo} from '../../src/api-node/addresses';
+import {isNullableStringOrNumber, isStringOrNumber} from '../extendedMatcher'
 
 const api: ReturnType<typeof create> = create(NODE_URL);
 
@@ -45,7 +45,7 @@ it('Script info meta', async () => {
 });
 
 it('balance details', async () => {
-    const { address } = STATE.ACCOUNTS.SIMPLE;
+    const {address} = STATE.ACCOUNTS.SIMPLE;
     const balanceDetails = await api.addresses.fetchBalanceDetails(address);
 
     expect(balanceDetails.address).toBe(address);
@@ -54,3 +54,82 @@ it('balance details', async () => {
     expect(typeof balanceDetails.available).isStringOrNumber();
     expect(typeof balanceDetails.effective).isStringOrNumber();
 });
+
+it('balance confirmations', async () => {
+    const {address} = STATE.ACCOUNTS.SIMPLE;
+    const confirmations = 3;
+    const balanceConfirmations = await api.addresses.fetchBalanceConfirmations(address, confirmations);
+
+    expect(typeof balanceConfirmations.balance).isStringOrNumber();
+    expect(typeof balanceConfirmations.address).toBe('string');
+    expect(typeof balanceConfirmations.confirmations).toBe('number');
+    expect(balanceConfirmations.address).toBe(address);
+    expect(balanceConfirmations.confirmations).toBe(confirmations);
+});
+
+it('address by public key', async () => {
+    const {address, publicKey} = STATE.ACCOUNTS.SIMPLE;
+    const addressByPublicKey = await api.addresses.fetchPublicKey(publicKey);
+
+    expect(addressByPublicKey).toBe(address)
+});
+
+it('address balance', async () => {
+    const {address} = STATE.ACCOUNTS.SIMPLE;
+    const balance = await  api.addresses.fetchBalance(address);
+
+    expect(typeof balance.balance).isStringOrNumber();
+    expect(typeof balance.address).toBe('string');
+    expect(typeof balance.confirmations).toBe('number');
+    expect(balance.address).toBe(address);
+});
+
+it('address effective balance', async () => {
+    const {address} = STATE.ACCOUNTS.SIMPLE;
+    const balance = await  api.addresses.fetchEffectiveBalance(address);
+
+    expect(typeof balance.balance).isStringOrNumber();
+    expect(typeof balance.address).toBe('string');
+    expect(typeof balance.confirmations).toBe('number');
+    expect(balance.address).toBe(address);
+});
+
+it('effective balance confirmations', async () => {
+    const {address} = STATE.ACCOUNTS.SIMPLE;
+    const confirmations = 3;
+    const balanceConfirmations = await api.addresses.fetchEffectiveBalanceConfirmations(address, confirmations);
+
+    expect(typeof balanceConfirmations.balance).isStringOrNumber();
+    expect(typeof balanceConfirmations.address).toBe('string');
+    expect(typeof balanceConfirmations.confirmations).toBe('number');
+    expect(balanceConfirmations.address).toBe(address);
+    expect(balanceConfirmations.confirmations).toBe(confirmations);
+});
+
+it('wallet addressed', async () => {
+    const address = MASTER_ACCOUNT.ADDRESS;
+    const walletAddresses = await api.addresses.fetchAddresses();
+
+    expect(walletAddresses).toBeInstanceOf(Array);
+    expect(walletAddresses[0]).toBe(address);
+
+});
+
+it('validate address, valid address', async () => {
+    const {address} = STATE.ACCOUNTS.SIMPLE;
+    const validateAddress = await  api.addresses.fetchValidate(address);
+
+    expect(validateAddress.address).toBe(address);
+    expect(validateAddress.valid).toBe(true);
+});
+
+it('validate address, invalid address', async () => {
+    const {address} = STATE.ACCOUNTS.SIMPLE;
+    const invalidAddress = address.replace("3", "4");
+    const validateAddress = await  api.addresses.fetchValidate(invalidAddress);
+
+    expect(validateAddress.address).toBe(invalidAddress);
+    expect(validateAddress.valid).toBe(false);
+});
+
+
