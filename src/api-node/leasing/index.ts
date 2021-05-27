2a6fefe3e25200b7bc1688a5ae2ff9853bee3d98
Daniil <lobanovdanil97@gmail.com>
2021-05-28 00:13:40 +0300

    changed fetchActive response type

--- src/api-node/leasing/index.ts
+++ src/api-node/leasing/index.ts
@@ -7,12 +7,16 @@ import request from '../../tools/request';
  * GET /leasing/active/{address}
  * Get all active leases for an address
  */
-export function fetchActive(base: string, address: string, options: RequestInit = Object.create(null)): Promise<Array<LeaseTransaction<TLong> & WithApiMixin & IActive>> {
+export function fetchActive(base: string, address: string, options: RequestInit = Object.create(null)): Promise<Array<LeasingObjectResponse<TLong> & WithApiMixin>> {
     return request({base, url: `/leasing/active/${address}`, options});
 }
 
-export interface IActive {
-    feeAssetId: string | null;
+export type LeasingObjectResponse<Long> = {
+    id: string,
+    originTransactionId: string,
+    recipient: string,
+    amount: Long,
+    status: string
 }
 
 /**
