dc6c4490017b5f0f61b153c2aec24b8420d218dc
inazarov <inazarov@web3tech.ru>
2021-09-15 15:43:54 +0300

    test

--- src/tools/adresses/eth2waves.ts
+++ src/tools/adresses/eth2waves.ts
@@ -1,15 +1,10 @@
-import { keccak, blake2b, base58Encode, stringToBytes } from '@waves/ts-lib-crypto'
-/*
-    eth 0x11242d6ec6B50713026a3757cAeb027294C2242a
-    waves 3EzjTrzQB57shiN4RwUi9ikC44FBGRzZ81G
-    chainId - C
-*/
+import { keccak, blake2b, base58Encode, base16Decode } from '@waves/ts-lib-crypto'
 
 export default function eth2waves(ethAddress: string, chainId: number): string {
     const prefixBytes = new Uint8Array([0x01, chainId]);
 
-    // Раскодировать HEX строку в байты (PK_HASH)                     
-    const pkHashBytes = stringToBytes(ethAddress);                          
+    // Раскодировать HEX строку в байты (PK_HASH)
+    const pkHashBytes = base16Decode(ethAddress);
 
     // Сделать чексумму CHECKSUM=keccak256(blake2b256([0x01, CHAIN_ID] + PK_HASH))
     const checksumBytes = new Uint8Array([
