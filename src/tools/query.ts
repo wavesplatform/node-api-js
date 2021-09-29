1a9343b29993369dab6dd98ea03d4328b1589e84
Daniil <lobanovdanil97@gmail.com>
2021-09-29 15:37:40 +0300

    fix

--- src/tools/query.ts
+++ src/tools/query.ts
@@ -6,7 +6,7 @@ export default function <T extends Record<string, any>>(params: T, evolver: TEvo
         .map<[keyof T, T[keyof T]]>(key => [key as keyof T, params[key] as T[keyof T]])
         .map(([key, value]) => [key, Object.prototype.hasOwnProperty.call(evolver, key) ? (evolver[key] as (data: T[keyof T]) => string | undefined)(value) : value])
         .filter(([key, value]) => value != null)
-        .map(([key, value]) => toArray(value).map(v => `${key}=${v}`))
+        .map(([key, value]) => toArray(value).map(v => `${key}=${v}`).join('&'))
         .join('&');
     return query.length ? `?${query}` : '';
 };
